47360f11fafd79b4d6337836d307262d3329d358
Kayane Rocha <kayannerch@gmail.com>
2024-12-16 22:34:05 -0300

    refactor: redirecioando a url raiz para a listagem de vagas

--- weong/weong/urls.py
+++ weong/weong/urls.py
@@ -18,8 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from django.urls import path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vaga/', include('vaga.urls'))
-]
+    path('vagas/', include('vaga.urls')),
+    path('', RedirectView.as_view(url='/vagas/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
