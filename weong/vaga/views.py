de701b1c002b81278e1c72e685333355ee453d40
Kayane Rocha <kayannerch@gmail.com>
2024-12-16 22:32:44 -0300

    feat: buscando todas as vagas em aberto

--- weong/vaga/views.py
+++ weong/vaga/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from vaga.models import Vaga
 
 # Create your views here.
+
+def index(request):
+    '''View para a página home com listagem de vagas'''
+    vagas = Vaga.objects.filter(preenchida__exact=0)
+
+    context = {
+        'vagas': vagas
+    }
+
+    return render(request, 'index.html', context=context)
