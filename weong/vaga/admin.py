f5d27a5d2109018b50e8ef95b1e2244ee2f97533
Kayane Rocha <kayannerch@gmail.com>
2024-12-16 22:31:11 -0300

    feat: personalizando a página de adm de vagas

--- weong/vaga/admin.py
+++ weong/vaga/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from vaga.models import Vaga
 
 # Register your models here.
+
+# admin.site.register(Vaga)
+
+@admin.register(Vaga)
+class VagaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'descricao', 'requisitos', 'local', 'preenchida')
+    list_filter = ('titulo', 'local', 'preenchida')
