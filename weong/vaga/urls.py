8ac28142fc8ae6956384c9777bf108eddfb673e9
Kayane Rocha <kayannerch@gmail.com>
2025-01-17 21:46:30 -0300

    refac: ajuste nas urls de vaga

--- weong/vaga/urls.py
+++ weong/vaga/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from vaga import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('vagas/<int:pk>', views.DetalheVagaView.as_view(), name='detalhe-vaga'),
+    path('', views.ListaVagasView.as_view(), name='index'),
+    path('lista-vagas/', views.ListaVagasView.as_view(), name='lista-vagas'),
+    path('detalhe-vaga/<int:pk>', views.DetalheVagaView.as_view(), name='detalhe-vaga'),
 ]
