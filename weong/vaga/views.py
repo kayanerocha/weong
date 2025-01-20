d62021eefec3f6a8c4c3e2546bbfac5a3cba4009
Kayane Rocha <kayannerch@gmail.com>
2025-01-19 21:47:49 -0300

    feat: cadastro de vaga

--- weong/vaga/views.py
+++ weong/vaga/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.views import generic
-from vaga.models import Vaga
+from django.utils.translation import gettext_lazy as _
+
+from .models import Vaga
+from .forms import VagaForm
+from usuario.forms import CadastroEnderecoForm
 
 class ListaVagasView(generic.ListView):
     model = Vaga
@@ -18,3 +23,25 @@ class ListaVagasView(generic.ListView):
 
 class DetalheVagaView(generic.DetailView):
     model = Vaga
+
+def cadastro_vaga(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    
+    if request.method == 'POST' and request.user.is_authenticated:
+        form_vaga = VagaForm(request.POST)
+        form_endereco = CadastroEnderecoForm(request.POST)
+
+        if form_vaga.is_valid() and form_endereco.is_valid():
+            endereco = form_endereco.save()
+            vaga = form_vaga.save(commit=False)
+            vaga.endereco = endereco
+            vaga.save()
+            return redirect('detalhe-vaga', pk=vaga.pk)
+        else:
+            messages.error(request, _('Cadastro de vaga não realizado.'))
+    else:
+        form_vaga = VagaForm()
+        form_endereco = CadastroEnderecoForm()
+
+    return render(request, 'vaga/cadastro.html', {'form_vaga': form_vaga, 'form_endereco': form_endereco})
