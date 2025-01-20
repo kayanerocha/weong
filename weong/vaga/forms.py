e88ede8862dac54e5c84c68d2caf995a928b93fe
Kayane Rocha <kayannerch@gmail.com>
2025-01-19 21:42:54 -0300

    feat: form de vaga

---
+++ weong/vaga/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+from .models import Vaga
+
+class VagaForm(forms.ModelForm):
+    class Meta:
+        model = Vaga
+        fields = ['titulo', 'descricao', 'requisitos']
