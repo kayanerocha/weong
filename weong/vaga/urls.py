81de8a15453a570ca14b387c19fac1f91993ee72
Kayane Rocha <kayannerch@gmail.com>
2025-01-22 21:19:00 -0300

    refac: alterando o nome da função que cria vaga

--- weong/vaga/urls.py
+++ weong/vaga/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.ListaVagasView.as_view(), name='index'),
     path('lista-vagas/', views.ListaVagasView.as_view(), name='lista-vagas'),
     path('detalhe-vaga/<int:pk>', views.DetalheVagaView.as_view(), name='detalhe-vaga'),
-    path('cadastro-vaga/', views.cadastro_vaga, name='cadastro-vaga'),
+    path('cadastro-vaga/', views.cadastrar_vaga, name='cadastro-vaga'),
 ]
