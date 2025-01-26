079be61df1506e62677f9eb9bf46ccfdcaedf449
Kayane Rocha <kayannerch@gmail.com>
2025-01-26 16:53:32 -0300

    feat: listando as vagas e detalhes da ong logada

--- weong/vaga/urls.py
+++ weong/vaga/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from vaga import views
 
 urlpatterns = [
-    path('', views.ListaVagasView.as_view(), name='index'),
-    path('lista-vagas/', views.ListaVagasView.as_view(), name='lista-vagas'),
+    path('', views.VagaList.as_view(), name='index'),
+    path('lista-vagas/', views.VagaList.as_view(), name='lista-vagas'),
     path('detalhe-vaga/<int:pk>', views.DetalheVagaView.as_view(), name='detalhe-vaga'),
-    path('cadastro-vaga/', views.cadastrar_vaga, name='cadastro-vaga'),
-    path('editar-vaga/<int:pk>', views.editar_vaga, name='edita-vaga')
+    path('cadastro-vaga/', views.VagaCreate.as_view(), name='cadastro-vaga'),
+    path('editar-vaga/<int:pk>', views.editar_vaga, name='edita-vaga'),
+    # path('teste-cadastro/', views.VagaCreate.as_view()),
+    path('minhas-vagas/', views.MinhasVagasList.as_view(), name='minhas-vagas')
 ]
