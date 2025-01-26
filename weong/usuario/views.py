82dde060fb7c485c1f10d275d3911cbe06e31b53
Kayane Rocha <kayannerch@gmail.com>
2025-01-26 16:53:08 -0300

    remove: deletando importação não utilizadas

--- weong/usuario/views.py
+++ weong/usuario/views.py
@@ -1,5 +1,4 @@
 from django.contrib import messages
-from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.utils.translation import gettext_lazy as _
 from django.views import generic
