547f3b08ec424f6f7abf8333e036754694845f57
Natan de Souza <108808557+Natan-Souz@users.noreply.github.com>
2025-01-28 17:49:23 -0300

    feat:url para cadastro de voluntário

--- weong/usuario/urls.py
+++ weong/usuario/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from usuario import views
+from usuario.views import cadastro_voluntario
 
 urlpatterns = [
     path('cadastro-ong', views.cadastro_ong, name='cadastro-ong'),
+    path('cadastro-voluntario/', cadastro_voluntario, name='cadastro-voluntario'),
 ]
