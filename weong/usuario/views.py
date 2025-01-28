12cd78170896ac6aa20e38cc41add209d03487d9
Natan de Souza <108808557+Natan-Souz@users.noreply.github.com>
2025-01-28 17:17:07 -0300

    feat: view para cadastro de voluntário

--- weong/usuario/views.py
+++ weong/usuario/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render, redirect
 from django.utils.translation import gettext_lazy as _
 from django.views import generic
 
-from usuario.forms import CadastroUsuarioForm, CadastroOngForm, CadastroEnderecoForm
-from usuario.models import Ong
+from usuario.forms import CadastroUsuarioForm, CadastroOngForm, CadastroEnderecoForm, CadastroVoluntarioForm
+from usuario.models import Ong, Voluntario
 
 # Create your views here.
 
@@ -45,3 +45,45 @@ def cadastro_ong(request):
 class CadastroOngView(generic.CreateView):
     model = Ong
     form_class = CadastroOngForm
+
+def cadastro_voluntario(request):
+    if request.method == 'POST':
+        form_usuario = CadastroUsuarioForm(request.POST)
+        form_voluntario = CadastroVoluntarioForm(request.POST)
+        form_endereco = CadastroEnderecoForm(request.POST)
+
+        if form_usuario.is_valid() and form_voluntario.is_valid() and form_endereco.is_valid():
+            # Criar o usuário
+            usuario = form_usuario.save(commit=False)
+            usuario.set_password(form_usuario.cleaned_data['password'])
+            usuario.is_active = False
+            usuario.save()
+
+            # Criar o endereço
+            endereco = form_endereco.save()
+
+            # Criar o voluntário
+            voluntario = form_voluntario.save(commit=False)
+            voluntario.usuario = usuario
+            voluntario.endereco = endereco
+            voluntario.save()
+
+            messages.success(request, _('Cadastro realizado com sucesso!'))
+            return redirect('/')
+        else:
+            messages.error(request, _('Erro no cadastro. Verifique os dados informados.'))
+    else:
+        form_usuario = CadastroUsuarioForm()
+        form_voluntario = CadastroVoluntarioForm()
+        form_endereco = CadastroEnderecoForm()
+
+    #Renderizar endereco
+    return render(request, 'voluntario.html', {
+        'form_usuario': form_usuario,
+        'form_voluntario': form_voluntario,
+        'form_endereco': form_endereco,
+    })
+
+class CadastroVoluntaioView(generic.CreateView):
+    model = Voluntario
+    form_class = CadastroVoluntarioForm
