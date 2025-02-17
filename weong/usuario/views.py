ee4abd5e757e6b443fb1b8722747a6dfcb4fca8d
Kayane Rocha <kayannerch@gmail.com>
2025-02-17 19:57:30 -0300

    bugfix: ajustando template de cadastro de ong

--- weong/usuario/views.py
+++ weong/usuario/views.py
@@ -39,7 +39,7 @@ def cadastro_ong(request):
         form_usuario = CadastroUsuarioForm()
         form_ong = CadastroOngForm()
         form_endereco = CadastroEnderecoForm()
-    return render(request, 'registration/ong.html', {
+    return render(request, 'registration/cadastro-ong.html', {
         'form_usuario': form_usuario,
         'form_ong': form_ong,
         'form_endereco': form_endereco,
