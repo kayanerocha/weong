fc89084b83b1339591be33daa6342bcf14a1a907
Kayane Rocha <kayannerch@gmail.com>
2025-03-11 21:28:09 -0300

    feat: add fim de candidaturas

--- weong/vaga/forms.py
+++ weong/vaga/forms.py
@@ -1,3 +1,4 @@
+import datetime
 from django import forms
 
 from .models import Vaga
@@ -7,7 +8,7 @@ class VagaForm(forms.ModelForm):
     descricao = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}), max_length=5000, label='Descrição da vaga')
     requisitos = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}), max_length=2500, label='Requisitos da vaga')
     quantidade_vagas = forms.IntegerField(max_value=100, label='Quantidade de vagas')
-    fim_candidaturas = forms.CharField(widget=forms.DateField, label='Encerramento das candidaturas')
+    fim_candidaturas = forms.DateField(initial=datetime.date.today, widget=forms.TextInput(attrs={'type':'date'}))
     class Meta:
         model = Vaga
         fields = ['titulo', 'descricao', 'requisitos', 'quantidade_vagas', 'fim_candidaturas']
