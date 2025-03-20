44f9d43bda4724d28030fd38e9db3e3541859b6f
Natan-Souz <natan-oliveira20111@hotmail.com>
2025-03-19 22:21:00 -0300

    refac: criado tratamento de erros para a validação da idade

--- weong/usuario/forms.py
+++ weong/usuario/forms.py
@@ -118,18 +118,26 @@ class CadastroVoluntarioForm(forms.ModelForm):
         return telefone
 
     def clean_data_nascimento(self):
-        data_nascimento = self.cleaned_data.get('data_nascimento')
-        if not data_nascimento:
-            raise ValidationError(_('Data de nascimento obrigatória.'), code='obrigatorio')
+        try:
+            data_nascimento = self.cleaned_data.get('data_nascimento')
+            if not data_nascimento:
+                raise ValidationError(_('Data de nascimento obrigatória.'), code='obrigatorio')
 
-        # Calcula a idade
-        hoje = date.today()
-        idade = hoje.year - data_nascimento.year - ((hoje.month, hoje.day) < (data_nascimento.month, data_nascimento.day))
+            hoje = date.today()
+            idade = hoje.year - data_nascimento.year
 
-        if idade < 18:
-            raise ValidationError(_('Você deve ter pelo menos 18 anos para se cadastrar.'), code='idade_insuficiente')
+            if (hoje.month, hoje.day) < (data_nascimento.month, data_nascimento.day):
+                idade -= 1
+            if idade < 18:
+                raise ValidationError(_('Você deve ter pelo menos 18 anos para se cadastrar.'), code='idade_insuficiente')
 
-        return data_nascimento
+            return data_nascimento
+
+        except ValidationError as e:
+            raise e  # Se for um erro esperado, apenas repassa
+
+        except Exception as e:
+            raise ValidationError(_('Erro ao validar a data de nascimento. Tente novamente.'), code='erro_interno')
 
 class EditarOngForm(forms.ModelForm):
     class Meta:
